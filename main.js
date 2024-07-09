5a99159f77a7b4139f03ff121d317d4ad2941e58
jinhyo park <pjhark0@gmail.com>
2024-07-10 00:02:47 +0900

    sss

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const API_KEY = `808d56a477c5434cb58b497bc9884b68`;
+// const API_KEY = `808d56a477c5434cb58b497bc9884b68`;
 let news = date.articles;
 const getLatesNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://idyllic-kelpie-95350e.netlify.app/top-headlines`
   );
   const response = await fetch(url);
   const date = await response.json();
